f4675e09470b569bc8fc2e3395df704e9249bf7d
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-02-20 09:42:00 +0100

    signal handler

--- parsing/read_input.c
+++ parsing/read_input.c
@@ -6,15 +6,33 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 09:13:12 by anfouger          #+#    #+#             */
-/*   Updated: 2026/02/20 09:19:56 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/02/20 09:36:06 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+static void	handle_sigint(int sig)
+{
+	(void)sig;
+
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+static void	setup_signals(void)
+{
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);	
+}
+
 int read_input()
 {
 	char *input;
+
+	setup_signals();
 
 	while (1)
 	{
