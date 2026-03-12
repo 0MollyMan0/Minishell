733b18e63ab78e6573e6f640d8151ceba487e2a8
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-03-12 12:02:05 +0100

    unset done

--- builtin/unset.c
+++ builtin/unset.c
@@ -6,37 +6,43 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 14:22:21 by anfouger          #+#    #+#             */
-/*   Updated: 2026/03/12 11:19:26 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/03/12 12:00:32 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static	char **dup_tab_less_i(char **tab, int index)
+static void copy_tab_less_i(char **tab, char **new_tab, int index)
 {
-	char	**new_tab;
-	int		i;
-	int		j;
+	int i;
+	int j;
 
-	new_tab = malloc(sizeof(char *) * (tab_len(tab)));
-	if (!new_tab)
-		return (NULL);
 	i = 0;
 	j = 0;
 	while (tab[j])
 	{
-		if (i == index)
+		if (j == index)
+		{
 			j++;
+			continue;
+		}
 		new_tab[i] = ft_strdup(tab[j]);
 		if (!new_tab[i])
-		{
 			clean_tab(new_tab, i);
-			return (NULL);
-		}
 		i++;
 		j++;
 	}
 	new_tab[i] = NULL;
+}
+
+static char **dup_tab_less_i(char **tab, int index)
+{
+	char **new_tab;
+
+	new_tab = malloc(sizeof(char *) * tab_len(tab));
+	if (!new_tab)
+		return (NULL);
+	copy_tab_less_i(tab, new_tab, index);
 	free_tab(tab);
 	return (new_tab);
 }
@@ -46,9 +52,14 @@ static int	get_index(char **tab, char *key)
 	int	i;
 
 	i = 0;
-	while (ft_strncmp(tab[i], key, ft_strlen(key)) != 0)
+	while (tab[i])
+	{
+		if (ft_strncmp(tab[i], key, ft_strlen(key)) == 0
+			&& tab[i][ft_strlen(key)] == '=')
+			return (i);
 		i++;
-	return (i);
+	}
+	return (-1);
 }
 
 int	builtin_unset(t_minish *minish, char **argv)
@@ -56,6 +67,8 @@ int	builtin_unset(t_minish *minish, char **argv)
 	int		i;
 
 	i = get_index(minish->envp, argv[1]);
+	if (i == -1)
+		return (0);
 	minish->envp = dup_tab_less_i(minish->envp, i);
 	return (0);
 }
