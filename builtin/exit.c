9f767f024ff89af501138a051fccc1d069afd468
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-04-26 09:38:10 +0200

    fix exit priorities

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 14:22:43 by anfouger          #+#    #+#             */
-/*   Updated: 2026/04/24 09:50:51 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/04/26 09:32:44 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,8 +52,6 @@ static int	exit_not_child(t_minish *minish, char **argv)
 	long	arg;
 
 	write(2, "exit\n", 5);
-	if (argv[1] && argv[2])
-		return (exit_too_many_arg());
 	if (!argv[1])
 	{
 		free_all(minish, 1);
@@ -61,6 +59,8 @@ static int	exit_not_child(t_minish *minish, char **argv)
 	}
 	if (!is_code_exit(argv[1]) || !verif_long(argv[1]))
 		exit_numeric_error(minish, argv[1]);
+	if (argv[1] && argv[2])
+		return (exit_too_many_arg());
 	arg = ft_atol(argv[1]);
 	free_all(minish, 1);
 	exit((unsigned char)arg);
